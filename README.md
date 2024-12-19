27d314b5654f197c3ec6d64229a3ce8836f1e5d0
Kudriashova Anastasiia <kudriashova.ag@gmail.com>
2024-12-19 18:40:29 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+npm i
+npm start
+CTRL+C - зупинка
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <h1 className="welcome">Lorem, ipsum.</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </>
+  );
+}
+
+export default App;
